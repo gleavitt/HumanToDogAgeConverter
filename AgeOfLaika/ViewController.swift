dcd968d8417d6a727844a54c894cb0012649d49e
Geoff Leavitt <gleavitt@mac.com>
2015-03-06 18:11:27 -0800

    first commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var humanAgeTextField: UITextField!
+    @IBOutlet weak var dogYearsTextLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func convertButton(sender: UIButton)
+    {
+        let ageFromTextField = humanAgeTextField.text.toInt()!
+        dogYearsTextLabel.hidden = false
+        
+        let conversionConstant = ageFromTextField * 7
+        
+        dogYearsTextLabel.text = "You are " + "\(ageFromTextField + conversionConstant)" + " in dog years."
+        
+        humanAgeTextField.text = ""
+        humanAgeTextField.resignFirstResponder()
+    }
+    
 }
 
